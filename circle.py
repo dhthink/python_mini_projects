213044b0498e82eaaed76762d83fc67e718cbb98
Dave H <davehaloski@gmail.com>
2026-02-26 16:07:01 -0500

    Undated mini projects

--- circle.py
+++ circle.py
@@ -3,10 +3,41 @@
 # def area_of_circle(r):
 #     return math.pi * r ** 2
 
-# diameter = float(input("What is the diameter of the Circle? "))
+# diameter = float(input("What is the diameter of the Circle?  "))
 # radius = diameter / 2
 # print("Radius:", radius)
 # print("Area of the circle is " + str(area_of_circle(radius)))
+
+# import math
+# #Curcumference = 2 * pi * r
+# def circumference_cir(r):
+#     return 2 * math.pi * r
+# diameter = float(input("What is the diameter of the Circle?" ))
+# radius = diameter / 2
+# print("Radius:" , radius)
+# print("Circumference of a circle is " + str(circumference_cir(radius)))
+
+# import math
+# # volume of sphere is 4/3 times pi r cubed or to the third power
+# def volume_bucket(radius, height):
+#     return  math.pi * radius ** 2 * height
+# diameter = float(input("What is the diameter of the circle in inches: "))
+# height = round(float(input("What is height of bucket in inches: " )),2)
+# radius = diameter / 2
+# print("Radius: ", radius )
+# print("Height", height)
+# print (f"Volume of bucket  is :  {volume_bucket(radius,height):.2f}")
+
+# painting a round building..square feet
+import math
+def area_round_building(radius, height):
+    return 2 * math.pi * radius * height
+diameter = float(input("What is the diameter of the circle in feet:"))
+height = round(float(input("Wht is the height of building in feet: ")),2)
+radius = diameter / 2
+print(f"Radius: {radius:.2f}")
+print(f"Height: {height:.2f}")
+print(f"Area of round building is : {area_round_building(radius,height):.2f} cubic feet")
 
 # import math
 # def area_rectangle(l, w):
@@ -32,9 +63,12 @@
 # # print("Percent of Free Throws is ", ft_perc(m,a), ".2f")
 # print("Percent of Free Throws is %.2f" % ft_perc(m,a))
 
-import math
-def miles_per_gallon (miles,gallons):
-    return miles/gallons
-miles = float(input("Enter the miles driven: "))
-gallons = float(input("Enter the gallons used: "))
-print("Miles per gallon is %.2f" % miles_per_gallon(miles, gallons))
+# import math
+# def miles_per_gallon (miles,gallons):
+#     return miles/gallons
+# miles = float(input("Enter the miles driven: "))
+# gallons = float(input("Enter the gallons used: "))
+# print("Miles per gallon is %.2f" % miles_per_gallon(miles, gallons))
+
+# import math
+# def volume_circle

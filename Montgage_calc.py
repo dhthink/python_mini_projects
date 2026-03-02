2f75c893988d1217f724c0da4f18fdacf0312375
Dave H <davehaloski@gmail.com>
2026-03-02 12:37:50 -0500

    tweek for leap_year

--- Montgage_calc.py
+++ Montgage_calc.py
@@ -1,12 +1,9 @@
 # This is my python code:
 def monthly_payment(principal, annual_rate, years):
-
-    # convert to decimal    
-    r_annual_rate = annual_rate / 100
-    # monthly interest rate
-    r = r_annual_rate / 12
-    #  total number of payments
-    n = years * 12
+ 
+    r_annual_rate = annual_rate / 100 # convert to decimal   
+    r = r_annual_rate / 12  # monthly interest rate
+    n = years * 12      #  total number of payments
     # handle zero loans
     if r == 0:
         return principal / n
@@ -14,8 +11,9 @@ def monthly_payment(principal, annual_rate, years):
     return principal * (r * (1 + r)**n) / ((1 + r)**n -1)
 
 def monthly_payment_credit(principal, annual_rate, years):
-    ear = (1 + annual_rate / 100 / 365) ** 365 - 1
-    r = (1 + ear) ** (1/12) - 1
+    # Effective Annual Rate as figured by Credit Card Companies
+    ear = (1 + annual_rate / 100 / 365.25) ** 365 - 1  # ear from google search
+    r = (1 + ear) ** (1/12) - 1  # monthly rate from google search
     n = years * 12
     if r == 0:
         return principal / n
@@ -37,11 +35,8 @@ if __name__ == "__main__":
     total_mortgage = payment_mortgage * years * 12
     total_credit = payment_credit * years * 12
 
-    print(f"\nFor a ${principal:,.2f} loan at {annual_rate:.3f}% for {years} years:")
-    print(f"Monthly payment (principal & interest): ${payment:,.2f}")
 
-    print(f"\nFor a ${principal:,.2f} loan at {annual_rate:.2f}% for {years} years:")
-    print(f"\n--- Standard Mortgage (Monthly Compounding) ---")
+    print(f"\n -- Standard Mortgage Calulation--")
     print(f"Monthly payment:          ${payment_mortgage:,.2f}")
     print(f"Total paid over 30 years: ${total_mortgage:,.2f}")
     print(f"\n--- Credit Card Style (Daily Compounding, 360 Payments) ---")
